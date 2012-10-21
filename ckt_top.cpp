44d7f708fb3a8810897d5432ee3a6fc8dc8e63f8
Ting <yuting20031918@gmail.com>
2012-10-21 16:03:21 -0500

    add ckt_top into filelist.

---
+++ ckt_top.cpp
@@ -0,0 +1,10 @@
+#include "ckt_top.h"
+
+CKT_TOP::CKT_TOP(string _name){
+	ckt1 = NULL;
+	ckt2 = NULL;
+	name = _name;
+}
+
+CKT_TOP::~CKT_TOP(){
+}
---
+++ ckt_top.h
@@ -0,0 +1,30 @@
+// ----------------------------------------------------------------//
+// Filename : ckt_top.h
+// Author : Ting Yu <tingyu1@illinois.edu>
+//
+// declaration of sub_ckt class
+// ----------------------------------------------------------------//
+
+#ifndef __SUB_CKT_H__
+#define __SUB_CKT_H__
+#include <fstream>
+#include "triplet.h"
+#include "global.h"
+#include "vec.h"
+#include "net.h"
+#include "util.h"
+#include "cholmod.h"
+#include "circuit.h"
+using namespace std;
+
+class CKT_TOP{
+public:
+	// stores the global and local circuit of each type circuit
+	Circuit* ckt1;
+	Circuit* ckt2;
+        CKT_TOP(string name);
+	~CKT_TOP();
+
+	string name;
+};
+#endif
